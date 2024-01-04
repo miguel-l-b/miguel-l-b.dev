128752a5fcf14e96523909dde075b35b73191043
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-04 16:58:26 -0300

    Add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,63 @@
+import { Link } from "react-router-dom"
+
+export interface HeaderProps {
+  path?: "home" | "books" | "posts" | "projects"
+}
+
+interface HeaderLinkProps {
+  className?: string
+  path: string
+  clicked: boolean
+  children: React.ReactNode
+}
+
+export function HeaderLink(props: HeaderLinkProps): JSX.Element {
+  return (
+    <Link
+      className={`flex group items-center h-full pl-10 pr-10 rounded-xl bg-none hover:bg-gradient-to-tr
+        hover:to-[#35423E] hover:from-[#3D3A47] ${
+          props.clicked && "bg-gradient-to-tr to-[#35423E] from-[#3D3A47]"
+        }`}
+      to={props.path}
+    >
+      <h2
+        className={`text-white font-jura text-2xl group-hover:bg-gradient-to-b hover:from-[#fff]
+        hover:to-[#ffffff00] hover:from-50% hover:bg-clip-text hover:text-transparent ${
+          props.clicked &&
+          "bg-gradient-to-b from-[#fff] to-[#ffffff00] from-50% bg-clip-text text-transparent"
+        }`}
+      >
+        {props.children}
+      </h2>
+    </Link>
+  )
+}
+
+export default function Header(props: HeaderProps): JSX.Element {
+  return (
+    <nav className="ml-20 mr-20 flex p-10 items-center justify-center">
+      {props.path !== "home" ? (
+        <Link to="/">
+          <img
+            className="w-50 h-50 relative left-[-10rem]"
+            src="logo-polygon.png"
+            alt="logo tipo"
+          />
+        </Link>
+      ) : (
+        <div className="block w-50 h-50 relative left-[-10rem] bg-white">a</div>
+      )}
+      <ul className="flex gap-20 justify-center bg-black-light w-1/2 h-14 rounded-full">
+        <HeaderLink path="/books" clicked={props.path === "books"}>
+          Livros
+        </HeaderLink>
+        <HeaderLink path="/posts" clicked={props.path === "posts"}>
+          Posts
+        </HeaderLink>
+        <HeaderLink path="/projects" clicked={props.path === "projects"}>
+          Projetos
+        </HeaderLink>
+      </ul>
+    </nav>
+  )
+}
