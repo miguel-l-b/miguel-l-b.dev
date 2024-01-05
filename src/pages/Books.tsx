9556850eee678166b8ae7a521b1de7382ea0609f
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-05 15:18:05 -0300

    Add main content to Books page

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -4,6 +4,9 @@ export default function Books(): JSX.Element {
   return (
     <>
       <Header path="books" />
+      <main className="text-center">
+        <h1>Books</h1>
+      </main>
     </>
   )
 }
