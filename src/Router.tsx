049d2b42bf3edf19d69d3003f5e33e78b8cd3942
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-05 11:29:01 -0300

    Add Course component and cursor animation

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,20 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
+import Course from "./components/Course"
 import Books from "./pages/Books"
 import Home from "./pages/Home"
 
 export default function Router(): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/books" element={<Books />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Course />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
---
+++ src/components/Course.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+export default function Course(): JSX.Element {
+  window.addEventListener("mousemove", (e) => {
+    document.getElementById("cursor-outline")!.style.transform =
+      `translate(calc(${e.clientX}px - 0.2rem), calc(${e.clientY}px - 0.2rem))`
+    document.getElementById("cursor-dot")!.animate(
+      {
+        left: `calc(${e.clientX}px - 0.8rem)`,
+        top: `calc(${e.clientY}px - 0.8rem)`,
+      },
+      { duration: 500, fill: "both" },
+    )
+  })
+  return (
+    <>
+      <div
+        id="cursor-dot"
+        className="fixed t-0 l-0 z-[1] w-7 h-7 pointer-events-none border-inherit border-2 rounded-full"
+      ></div>
+      <div
+        id="cursor-outline"
+        className="fixed t-0 l-0 z-[1] pointer-events-none w-2 h-2 bg-white rounded-full"
+      ></div>
+    </>
+  )
+}
