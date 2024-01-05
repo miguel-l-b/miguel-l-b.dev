04c781892307c05eb8f9c2ddb7c3cece10af6ccd
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-05 11:29:14 -0300

    Update Home page layout and content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,27 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Header path="home" />
-      <h1>Home</h1>
+      <main className="text-center mt-20">
+        <img
+          className="w-52 h-52 m-auto"
+          src="logo-ellipse.svg"
+          alt="logo tipo"
+        />
+        <br />
+        <h1>🧑‍💻 Desenvolvendo aplicações Web e Mobile desde 2020</h1>
+        <h1>
+          🎓 Cursando Desenvolvimento de Sistemas pelo{" "}
+          <a
+            className="animated-link"
+            href="https://cotuca.unicamp.br/"
+            target="_blank"
+          >
+            COTUCA - Unicamp
+          </a>
+        </h1>
+        <h1>🧐 Sempre</h1>
+        <h1>🚀 Buscando oportunidade de estágio</h1>
+      </main>
     </>
   )
 }
