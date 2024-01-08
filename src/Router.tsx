34085b3f73cec331c02b56607e9cf78a545e7f0f
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-08 11:26:01 -0300

    Add Projects page to Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import Curse from "./components/Curse"
 import ErrorNotFound from "./pages/404"
 import Books from "./pages/Books"
 import Home from "./pages/Home"
+import Projects from "./pages/Projects"
 
 export default function Router(): JSX.Element {
   return (
@@ -14,6 +15,7 @@ export default function Router(): JSX.Element {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/books" element={<Books />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/*" element={<ErrorNotFound />} />
         </Routes>
       </BrowserRouter>
