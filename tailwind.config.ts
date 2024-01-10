b747c69071741950f5314c73483412524e13a3e4
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-10 09:11:32 -0300

    Add new fonts and colors to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,29 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        "roboto": ['Roboto Mono', "monospace"],
+        "jura": ['Jura', 'sans-serif'],
+
       },
+      colors: {
+        "black-dark": "#161818",
+        "black": "#262727",
+        "black-light": "#282929",
+        "gray-dark": "#4A5560",
+        "gray": "#AAB0BE",
+        "gray-light": "#D0DFDE",
+        "blue-dark": "#2F4572",
+        "blue": "#679EDE",
+        "blue-light": "#BED7E4",
+        "yellow": "#EDDE73",
+        "yellow-light": "#E1DDB7",
+        "green": "#23AC57",
+        "green-light": "#8FD3A6",
+        "red": "#D24F4F",
+        "red-light": "#C97878",
+        "white": "#E6EEEF",
+      }
     },
   },
   plugins: [],
