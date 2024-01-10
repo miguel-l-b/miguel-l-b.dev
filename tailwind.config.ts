81b13e8881ea5fd1dca856cfbd3ff2a8f4857b2e
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-10 09:03:05 -0300

    convert next.js no src

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,35 +2,17 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      fontFamily: {
-        "roboto": ['Roboto Mono', "monospace"],
-        "jura": ['Jura', 'sans-serif'],
-
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      colors: {
-        "black-dark": "#161818",
-        "black": "#262727",
-        "black-light": "#282929",
-        "gray-dark": "#4A5560",
-        "gray": "#AAB0BE",
-        "gray-light": "#D0DFDE",
-        "blue-dark": "#2F4572",
-        "blue": "#679EDE",
-        "blue-light": "#BED7E4",
-        "yellow": "#EDDE73",
-        "yellow-light": "#E1DDB7",
-        "green": "#23AC57",
-        "green-light": "#8FD3A6",
-        "red": "#D24F4F",
-        "red-light": "#C97878",
-        "white": "#E6EEEF",
-      }
     },
   },
   plugins: [],
