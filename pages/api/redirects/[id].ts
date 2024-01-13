fb519c39ba7e24f124aa7791a53264401bb78d9a
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-13 13:21:35 -0300

    Add deleteHandle function and import validToken utility

--- pages/api/redirects/[id].ts
+++ pages/api/redirects/[id].ts
@@ -1,6 +1,7 @@
 import { withLogging } from "@/infra/middlewares"
 import { NotFoundRedirectError } from "@/infra/models/responses"
 import withErrorInternal from "@/infra/utils/error"
+import validToken from "@/infra/utils/valid_token"
 import { kv } from "@vercel/kv"
 import { NextApiRequest, NextApiResponse } from "next"
 import { createRouter } from "next-connect"
@@ -8,6 +9,7 @@ import { createRouter } from "next-connect"
 const router = createRouter<NextApiRequest, NextApiResponse>()
   .use(withLogging)
   .get(getHandle)
+  .delete(deleteHandle)
 
 async function getHandle(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query
@@ -20,6 +22,23 @@ async function getHandle(req: NextApiRequest, res: NextApiResponse) {
   if (!result)
     return res.status(404).json(NotFoundRedirectError(id as string))
   res.status(200).json(result)
+}
+
+async function deleteHandle(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query
+
+  if(!validToken(req, res))
+    return res.status(401).json({ error: "Unauthorized", message: "You are not authorized to perform this action." })
+
+  if (!id)
+    return res.status(404).json(NotFoundRedirectError("null"))
+
+  const result = await kv.del(id as string)
+
+  if (!result)
+    return res.status(404).json(NotFoundRedirectError(id as string))
+  res.status(200).json(result)
+
 }
 
 export default router.handler({ onError: withErrorInternal })
