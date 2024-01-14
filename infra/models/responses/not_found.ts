16a3a4a3b0d7784651268718aff289a0abb2346a
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-14 13:02:05 -0300

    Add NotFoundTimelineError function

--- infra/models/responses/not_found.ts
+++ infra/models/responses/not_found.ts
@@ -8,6 +8,11 @@ export const NotFoundRedirectError = (id: string) => ({
   message: `The redirect ${id} is not found.`
 })
 
+export const NotFoundTimelineError = (id: string) => ({
+  error: "Not found",
+  message: `The timeline ${id} is not found.`
+})
+
 export const NotFoundCustomMsgError = (message: string) => ({
   error: "Not found",
   message: message
