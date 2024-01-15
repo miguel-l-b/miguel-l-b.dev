1668f4952a3e8cbbd4764e2f6f3f83551b6ba039
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-15 19:38:36 -0300

    Refactor error handling in withErrorInternal function

--- infra/utils/error.ts
+++ infra/utils/error.ts
@@ -1,9 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import generateUUID from "./uuid"
 
 export default function withErrorInternal(error: unknown, req: NextApiRequest, res: NextApiResponse) {
-  const id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+  const id = generateUUID()
   console.error(`Error ID: ${id} > \n`, error)
-  return res.json(
+  return res.status(500).json(
     {
       error: "Internal Server Error",
       message: "Unfortunately you hear an unexpected error, if it happens again contact support and send the ID and IP below.",
