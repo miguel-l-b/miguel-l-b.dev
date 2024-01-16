dcedea1a53c69c290c8ae97bfd344644e71d0785
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-16 09:58:43 -0300

    Add next-sitemap configuration file and update package dependencies

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://miguellb.net',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
