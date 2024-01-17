b35973fa2d0a006ec13593163f6645c9d8078906
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-17 20:52:33 -0300

    Add dynamic page for displaying tech details

---
+++ pages/techs/[id].tsx
@@ -0,0 +1,44 @@
+import Header from "@/components/Header"
+import { techType } from "@/infra/models"
+import Head from "next/head"
+import Image from "next/image"
+import { useParams } from "next/navigation"
+import { useEffect, useState } from "react"
+
+export default function Projects(): JSX.Element {
+  const params = useParams()
+  const [tech, setTech] = useState<techType | undefined>(undefined)
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/techs/${params.id}`)
+      .then(res => res.json())
+      .then(data => setTech(data))
+  }, [params])
+
+  if(tech === undefined)
+    return (
+      <>
+        <Header path="/techs" />
+        <h1 className="text-center">Buscando...</h1>
+      </>
+    )
+  return (
+    <>
+      <Head>
+        <title>Miguel L B | {tech.name}</title>
+        <meta name="description" content={tech.description} />
+      </Head>
+      <Header path="/techs" />
+      <main className="flex flex-wrap items-center m-auto w-[65dvw] gap-12">
+        <Image
+            src={tech.img}
+            width={80}
+            height={80}
+            alt={`foto de ${tech.name}`}
+          />
+        <h1 className="text-2xl">{tech.name}</h1>
+        <p className="basis-full text-justify">{tech.description}</p>
+      </main>
+    </>
+  )
+}
