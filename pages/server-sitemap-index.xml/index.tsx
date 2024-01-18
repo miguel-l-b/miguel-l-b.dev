78322ff83edacef35160a9776ec354267ff044b3
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-18 11:11:19 -0300

    Add techs URLs to server-side sitemap index

--- pages/server-sitemap-index.xml/index.tsx
+++ pages/server-sitemap-index.xml/index.tsx
@@ -1,11 +1,16 @@
 import { getServerSideSitemapIndexLegacy } from 'next-sitemap'
 import { GetServerSideProps } from 'next'
+import { techType } from '@/infra/models'
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  // Method to source urls from cms
-  // const urls = await fetch('https//miguellb.net/api')
+  const techs = fetch('/api/techs/all')
+    .then(async (res) => await res.json() as Array<techType & { id: string }>)
+    .catch(() => [])
+
+  const techsUrl = (await techs).map(tech => `https://miguellb.net/techs/${tech.id}`)
 
   return getServerSideSitemapIndexLegacy(ctx, [
+    ...techsUrl
   ])
 }
 
