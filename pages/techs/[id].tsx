dfd584769acff920c5eab4dd42e96353ebfede8c
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-18 11:11:16 -0300

    Refactor techs/[id].tsx: Clean up imports and add links and icons

--- pages/techs/[id].tsx
+++ pages/techs/[id].tsx
@@ -1,19 +1,27 @@
-import Header from "@/components/Header"
-import { techType } from "@/infra/models"
 import Head from "next/head"
 import Image from "next/image"
-import { useParams } from "next/navigation"
+import Link from "next/link"
+import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
+import * as MaterialIcons from "react-icons/md"
+
+import Header from "@/components/Header"
+import { techType } from "@/infra/models"
 
 export default function Projects(): JSX.Element {
-  const params = useParams()
+  const params = useRouter().query
   const [tech, setTech] = useState<techType | undefined>(undefined)
 
   useEffect(() => {
-    fetch(`/api/techs/${params.id}`)
-      .then(res => res.json())
-      .then(data => setTech(data))
+    if(params.id !== undefined)
+      fetch(`/api/techs/${params.id}`)
+        .then(res => res.json())
+        .then(data => setTech(data))
   }, [params])
+
+  useEffect(() => {
+    console.log(tech)
+  }, [tech])
 
   if(tech === undefined)
     return (
@@ -31,12 +39,41 @@ export default function Projects(): JSX.Element {
       <Header path="/techs" />
       <main className="flex flex-wrap items-center m-auto w-[65dvw] gap-12">
         <Image
+            className="p-4 rounded-2xl bg-gray-dark shadow-lg shadow-black-dark"
             src={tech.img}
-            width={80}
-            height={80}
+            width={100}
+            height={100}
             alt={`foto de ${tech.name}`}
           />
         <h1 className="text-2xl">{tech.name}</h1>
+        <div className="flex gap-2 px-1 overflow-x-auto">
+          {
+            tech.tags?.map((tag, index) => (
+              <span key={index} className="bg-gray-dark px-2 my-1 rounded-md whitespace-nowrap">{tag}</span>
+            ))
+          }
+        </div>
+        <div className="flex gap-5">
+          {
+            tech.github && (
+              <Link className="bg-white rounded-full" href={tech.github} target="_blank">
+                <Image
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                  width={30}
+                  height={30}
+                  alt="github icon"
+                />
+              </Link>
+            )
+          }
+          {
+            tech.site && (
+              <Link href={tech.site} target="_blank">
+                <MaterialIcons.MdLink className="text-gray" size={30} />
+              </Link>
+            )
+          }
+        </div>
         <p className="basis-full text-justify">{tech.description}</p>
       </main>
     </>
