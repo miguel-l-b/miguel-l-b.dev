3f0c0ddaf4d47f86177ddabc0bd659bc252f4bf8
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-18 21:39:40 -0300

    Remove unused code in server-sitemap-index.xml

--- pages/server-sitemap-index.xml/index.tsx
+++ pages/server-sitemap-index.xml/index.tsx
@@ -3,14 +3,7 @@ import { GetServerSideProps } from 'next'
 import { techType } from '@/infra/models'
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const techs = fetch('/api/techs/all')
-    .then(async (res) => await res.json() as Array<techType & { id: string }>)
-    .catch(() => [])
-
-  const techsUrl = (await techs).map(tech => `https://miguellb.net/techs/${tech.id}`)
-
   return getServerSideSitemapIndexLegacy(ctx, [
-    ...techsUrl
   ])
 }
 
