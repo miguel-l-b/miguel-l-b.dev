2d103f2548a03960bc6ffc6aa63ea7b3a545c7e6
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-22 15:56:00 -0300

    Refactor ValidSchema function signature to use generic type

--- infra/utils/valid_schema.ts
+++ infra/utils/valid_schema.ts
@@ -1,11 +1,11 @@
 import { z } from "zod"
 
-export default async function ValidSchema(Schema: z.Schema, data: any) {
+export default async function ValidSchema<T>(Schema: z.Schema, data: T) {
   try {
-    return await Schema.safeParseAsync(data).then((result) => {
-      if(!result.success)
-        return false
+    return await Schema.parseAsync(data).then((result) => {
       return true
+    }).catch((err) => {
+      return false
     })
   } catch (error) {
     return false
