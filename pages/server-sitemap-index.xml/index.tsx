207a40cd576cb1b17124271c5f28d35242f35645
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-23 21:00:36 -0300

    Update API URL to use HTTPS

--- pages/server-sitemap-index.xml/index.tsx
+++ pages/server-sitemap-index.xml/index.tsx
@@ -3,7 +3,7 @@ import { GetServerSideProps } from 'next'
 import { TechType } from '@/infra/models/db/tech'
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const techs = await fetch(`http://${process.env.DOMAIN}/api/techs/keys`).then(async (e) => await e.json() as string[])
+  const techs = await fetch(`https://${process.env.DOMAIN}/api/techs/keys`).then(async (e) => await e.json() as string[])
   return getServerSideSitemapIndexLegacy(ctx, [
     ...techs.flatMap(e => `https://${process.env.DOMAIN}/techs/${e}`)
   ])
