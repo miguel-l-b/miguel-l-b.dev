ba77223c84e1b215841e20ef0300b690288bf989
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-24 16:29:33 -0300

    Add PUT endpoint for updating techs

--- pages/api/techs/[id].ts
+++ pages/api/techs/[id].ts
@@ -10,6 +10,7 @@ const router = createRouter<NextApiRequest, NextApiResponse>()
   .use(withLogging)
   .get(getHandle)
   .delete(deleteHandle)
+  .put(putHandle)
 
 async function getHandle(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query
@@ -37,6 +38,24 @@ async function deleteHandle(req: NextApiRequest, res: NextApiResponse) {
   try {
     await TechDB.delete(id as string)
     return res.status(200).json({ message: "Deleted" })
+  } catch (error) {
+    if(error instanceof ErrorKV)
+      if(error.code === ErrorKVCode.NotFound)
+        return res.status(404).json(NotFoundTechError(id as string))
+
+    withErrorInternal(error, req, res)
+  }
+}
+
+async function putHandle(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query
+
+  if (!id)
+    return res.status(404).json(NotFoundTechError("null"))
+
+  try {
+    const data = await TechDB.update(id as string, req.body)
+    return res.status(200).json(data)
   } catch (error) {
     if(error instanceof ErrorKV)
       if(error.code === ErrorKVCode.NotFound)
