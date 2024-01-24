4f805a55cd2480be5905b9c86ce46502ef21cc05
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-23 21:45:09 -0300

    Update base URL to use HTTPS

--- infra/utils/url.ts
+++ infra/utils/url.ts
@@ -3,7 +3,7 @@ const getBaseUrl = () => {
      return "http://localhost:3000"
    }
 
-  return process.env.NEXT_PUBLIC_VERCEL_URL
+  return "https://"+process.env.NEXT_PUBLIC_VERCEL_URL
 }
 
 export default getBaseUrl
