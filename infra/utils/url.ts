1d79f8f2033894e94ccf3d5c49fa89e568f3758f
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-23 22:13:14 -0300

    Update base URL in getBaseUrl function

--- infra/utils/url.ts
+++ infra/utils/url.ts
@@ -3,7 +3,7 @@ const getBaseUrl = () => {
      return "http://localhost:3000"
    }
 
-  return "https://"+process.env.NEXT_PUBLIC_VERCEL_URL
+  return "https://miguellb.net"
 }
 
 export default getBaseUrl
