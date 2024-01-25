86e824b3071b14d6bf4e9d8ad46c463943fe8ceb
miguel-l-b <miguellopesbraido@gmail.com>
2024-01-25 16:38:44 -0300

    Add server-side authentication check in Dashboard page

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,6 +1,22 @@
 import Footer from "@/components/Footer"
 import HeaderDash from "@/components/HeaderDash"
+import validToken from "@/infra/utils/valid_token"
+import { GetServerSidePropsContext, GetServerSidePropsResult } from "next"
 import Head from "next/head"
+
+export async function getServerSideProps({ req, res }: GetServerSidePropsContext): Promise<GetServerSidePropsResult<any>> {
+  if(!validToken(req, res))
+    return {
+      redirect: {
+        destination: "/dashboard/login",
+        permanent: false
+      }
+    }
+  return {
+    props: {}
+  }
+}
+
 
 export default function Dashboard() {
   return (
