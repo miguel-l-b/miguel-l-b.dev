c7e1b76e0298d50154c1cef7172289ed2e3a1688
miguel-l-b <miguellopesbraido@gmail.com>
2024-02-14 22:33:07 -0300

    feat: Add Project component and getServerSideProps function in `projects/index.tsx`

---
+++ components/Project.tsx
@@ -0,0 +1,9 @@
+import { ProjectType } from "@/infra/models/db/project"
+
+export default function Project(props: ProjectType): JSX.Element {
+  return (
+    <div>
+      <h1>Project</h1>
+    </div>
+  )
+}
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,10 +1,39 @@
 import Footer from "@/components/Footer"
 import Header from "@/components/Header"
+import Project from "@/components/Project"
+import { ProjectType } from "@/infra/models/db/project"
+import getBaseUrl from "@/infra/utils/url"
+import { GetServerSidePropsResult } from "next"
 
-export default function Projects(): JSX.Element {
+export async function getServerSideProps(): Promise<GetServerSidePropsResult<ProjectType[]>> {
+  const res = await fetch(`${getBaseUrl()}/api/projects/all`)
+  const errorCode = res.status >= 200 && res.status <= 399 ? false : res.status
+
+  if(errorCode != false)
+    return { props: [] }
+
+  const projects = await res.json() as ProjectType[]
+
+  return {
+    props: projects,
+  }
+}
+
+export default function Projects(props: ProjectType[]): JSX.Element {
+  if(!props)
+    return (
+      <>
+        <Header path="/projects" />
+        <p>There was an error fetching the projects.</p>
+        <Footer />
+      </>
+    )
   return (
     <>
       <Header path="/projects" />
+      {
+        props.map((item, index) => <Project key={index} {...item} />)
+      }
       <Footer />
     </>
   )
