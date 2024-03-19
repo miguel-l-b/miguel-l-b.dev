3e561ee2a43ba105f716dfef1438fd4f8ab11688
miguel <miguel.lopes@senhorcontabil.com.br>
2024-03-19 07:50:13 -0300

    [feat](to-do): using `contexts` instead of the DOM

---
+++ components/form/input.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface FromInputProps extends React.HTMLProps<HTMLInputElement> {
+
+}
+
+export default function FromInput({ ...props }: FromInputProps) {
+
+}
